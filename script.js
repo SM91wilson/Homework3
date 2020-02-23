71399d443bca87fa94d0edb2aa90efe7c400d699
tango <sm91wilson@gmail.com>
2020-02-23 14:18:07 -0500

    added function for random lower case letter

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var generateBtn = document.querySelector("#generate");
 // arrays for characters used for the password
 var lowArray = [...Array(26)].map((val, i) => String.fromCharCode(i + 97));
 var uppArray = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
-var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specArray = []
+var numArray = [...Array(10)].map((val, i) => String.fromCharCode(i + 48));
+var specArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","~","`","{","}","[","]","|","\\",":",";","\"","'","<",">","?","\/"]
 console.log(lowArray)
 console.log(uppArray)
 console.log(numArray)
+console.log(specArray)
+
+function randomLow() {
+  lowArray[Math.floor(Math.random() * lowArray.length)];
+}
+
 // Write password to the #password input
 function writePassword() {
   //(not a comment, need this!!!!) var password = generatePassword();
@@ -15,7 +21,7 @@ function writePassword() {
   // prompt for length of password
   var length = prompt("how long do you want your password? (between 8 & 128 characters)");
     if(length < 8){
-      alert("password too short");
+       alert("password too short");
     }else if(length > 128){
       alert("password too long");
     }else{
